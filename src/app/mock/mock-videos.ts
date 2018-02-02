6fa7c267fbe8edb10fdc5915cc0176fdc859ad85
gbesset <guillaume.besset.pro@gmail.com>
2018-02-02 18:05:35 +0100

    #6 Amelioration homePage avec composants bootstrap4

--- src/app/mock/mock-videos.ts
+++ src/app/mock/mock-videos.ts
@@ -2,5 +2,5 @@ import { Item } from '../contract/item';
 
 export const MOCK_VIDEOS: Item[] = [
   {id:1 , path: '/path/to/file/', name: 'video 1'},
-  {id:2 , path: '/path/to/file/', name: 'video number 2'}
+  {id:2 , path: '/path/to/file/', name: 'video number 2 dzd dz  z fzd z'}
 ];
